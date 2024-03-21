d1eca432b6a68b6c658d1f29294126ccbeed771d
Júlio Rafael <jmoisesrafael@gmail.com>
2024-03-21 19:50:01 +0200

    Create header.php

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <title> Dondza Academy</title>
+</head>
+<body>
+    <!-- Esta tag serve para definir o cabecalho-->
+    <header>
+        <h1>Dondza Academy</h1>
+        <!-- A tag nav serve para criar menus de navegacao -->
+        <nav>
+            <ul>
+                <li> <a href="index.html"> Inicio</a></li>
+                <li> <a href="sobre-nos.html"> Sobre nós</a></li>
+                <li> <a href="contactos.html"> Contactos</a></li>
+                <li> <a href="login.html"> Entrada </a></li>
+            </ul>
+        </nav>
+    </header>
