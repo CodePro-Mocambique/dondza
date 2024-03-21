fe3da32475378f790aef0c17d2ef04e07a1e8410
Júlio Rafael <jmoisesrafael@gmail.com>
2024-03-21 19:50:02 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,10 @@
+
+    <!-- Esta tag serve para definir o rodapé -->
+    <footer>
+        <p>&copy; 2024. Todos direitos reservados.</p>
+        <a href="https://codeprogroup.com" target="_blank"> CodePro</a>
+    </footer>
+
+</body>
+
+</html>
