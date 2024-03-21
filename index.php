9a3f6b2ecd35f879ed47312aeec99fb88dc49a29
Júlio Rafael <jmoisesrafael@gmail.com>
2024-03-21 19:49:57 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+    include_once 'header.php';
+?>
+    <!-- Esta tag serve para dividir as seccoes -->
+    <section class="courses-container">
+        <div class="course">
+            <img src="assets/img/webdev.png" alt="Curso de WebDev">
+            <h2>Curso de Desenvolvimento Web para Iniciantes </h2>
+            <p>Descrição breve do curso. Aprenda Desenvolvimento Web do zero e torne-se um desenvolvedor experiente. </p>
+            <p class="price"> 12.750 MZN </p>
+            <a href="detalhes-curso.html">Detalhes</a>
+        </div>
+        <div class="course">
+            <img src="assets/img/webdev.png" alt="Curso de WebDev">
+            <h2>Curso de Desenvolvimento Web para Iniciantes </h2>
+            <p>Descrição breve do curso. Aprenda Desenvolvimento Web do zero e torne-se um desenvolvedor experiente. </p>
+            <p class="price"> 12.750 MZN </p>
+            <a href="detalhes-curso.html">Detalhes</a>
+        </div>
+
+    </section>
+    
+<?php
+include_once 'footer.php';
+?>
